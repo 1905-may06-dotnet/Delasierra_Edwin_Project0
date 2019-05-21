9459e77d616c7066ca8048a5b4aba52399d6cef8
edls2013 <edls2013@tamu.edu>
2019-05-21 10:31:52 -0500

    Added PizzaBox.Domain directory

---
+++ PizzaBox.Domain/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaBox.Domain
+{ 
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public List<Order> orderHistory { get; set; }
+        public DateTime lastOrderTime { get; set; }
+        public bool recentlyOrdered { get; set; }
+
+        public User()
+        {
+            username = "guest";
+            password = "password";
+            recentlyOrdered = false;
+        }
+
+        public User(string name, string pw)
+        {
+            username = name;
+            password = pw;
+            recentlyOrdered = false;
+        }
+    }
+}
