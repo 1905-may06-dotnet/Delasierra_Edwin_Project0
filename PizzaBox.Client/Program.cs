4fdebc5e3e00fc1e70484545bd3b5ea0898143e4
edls2013 <edls2013@tamu.edu>
2019-05-21 16:04:12 -0500

    Allow user to register account and login

--- PizzaBox.Client/Program.cs
+++ PizzaBox.Client/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using PizzaBox.Domain;
+using PizzaBox.Data;
+using PizzaBox.Data.Model;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace PizzaBox.Client
 {
@@ -8,11 +12,43 @@ namespace PizzaBox.Client
         static void Main(string[] args)
         {
             Client client = new Client();
+            //Crud d = new Crud();
+            //var users = d.GetUsers();
+            string response1, response2;
+            BEGIN:
+            Console.WriteLine("Do you have an account? (y/n)");
+            response1 = Console.ReadLine();
+            if (response1.StartsWith("y"))
+                goto LOGIN;
+            
+            Console.WriteLine("Please create an account");
+            Console.Write("Create your username: ");
+            response1 = Console.ReadLine();
+            Console.Write("Create your password: ");
+            response2 = Console.ReadLine();
+            client.CreateNewUser(response1, response2);
+            goto BEGIN;
+
+            LOGIN:
+            Console.WriteLine("Please login");
+            Console.Write("Enter your username: ");
+            response1 = Console.ReadLine();
+            Console.Write("Enter your password: ");
+            response2 = Console.ReadLine();
+            if (client.LoginReturningUser(response1, response2))
+                Console.WriteLine("Login successful");
+            else
+            {
+                Console.WriteLine("Username or password is incorrect");
+                goto LOGIN;
+            }
+            /*
+            Client client = new Client();
             Console.WriteLine("Do you have an account? (y/n)");
             string response1, response2;
             response1 = Console.ReadLine();
-
-            login:
+            /*
+            LOGIN:
             if (response1.StartsWith('y'))
             {
                 Console.WriteLine("Please login");
@@ -23,7 +59,10 @@ namespace PizzaBox.Client
                 if (client.LoginReturningUser(response1, response2))
                     Console.WriteLine("Login successful");
                 else
+                {
                     Console.WriteLine("The username and password are incorrect");
+                    goto LOGIN;
+                }
             }
             else
             {
@@ -35,18 +74,33 @@ namespace PizzaBox.Client
                 client.CreateNewUser(response1, response2);
                 Console.WriteLine("New account created");
                 response1 = "y";
-                goto login;
+                goto LOGIN;
                 //Console.Write("Re-enter password: ");
             }
+            HOME:
+            Console.WriteLine("Select an action (enter the number)");
+            Console.WriteLine("1. Place order");
+            Console.WriteLine("2. View order history");
+            Console.WriteLine("3. Logout");
+            string homeselect = Console.ReadLine();
+            if (homeselect.StartsWith("1"))
+            {
+                Console.WriteLine("Select a location");
+                client.PrintLocations();
+                response1 = Console.ReadLine();
+            }
+            else if (homeselect.StartsWith("2"))
+            {
 
-            /*
-            Console.WriteLine("Please login");
-            string name = "Edwin";
-            string password = "1234";
-            client.CreateNewUser(name, password);
-            name = "wrong";
-            bool exists = client.LoginReturningUser(name, password);
-            Console.WriteLine($"This user exists: {exists}");
+            }
+            else
+            {
+                Console.WriteLine("Are you sure you want to logout? (y/n)");
+                response1 = Console.ReadLine();
+                if (response1.StartsWith("y"))
+                    goto LOGIN;
+                goto HOME;
+            }
             */
         }
     }
