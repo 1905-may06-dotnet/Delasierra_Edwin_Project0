3a6dfe3633737db13f8e767c608c8d92c12f5c1a
edls2013 <edls2013@tamu.edu>
2019-05-21 10:29:06 -0500

    Added PizzaBox.Client directory

---
+++ PizzaBox.Client/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PizzaBox.Domain;
+
+namespace PizzaBox.Client
+{
+    class Client
+    {
+        private Dictionary<string, User> userBase = new Dictionary<string, User>();
+        private Dictionary<int, Location> locationBase = new Dictionary<int, Location>();
+        private User currentUser;
+        public void CreateNewUser(string name, string pw)
+        {
+            User newUser= new User(name, pw);
+            userBase.Add(name, newUser);
+        }
+
+        public bool LoginReturningUser(string name, string pw)
+        {
+            foreach (KeyValuePair<string, User> u in userBase)
+            {
+                if (u.Key == name && u.Value.password == pw)
+                    return true;
+            }
+            return false;
+        }
+
+        public void AddLocation(int storeID, string name)
+        {
+            Location store = new Location(storeID, name);
+            locationBase.Add(storeID, store);
+        }
+
+        public void SetCurrentUser(User person)
+        {
+            currentUser = person;
+        }
+    }
+}
---
+++ PizzaBox.Client/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using PizzaBox.Domain;
+
+namespace PizzaBox.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            Console.WriteLine("Do you have an account? (y/n)");
+            string response1, response2;
+            response1 = Console.ReadLine();
+
+            login:
+            if (response1.StartsWith('y'))
+            {
+                Console.WriteLine("Please login");
+                Console.Write("Enter your username: ");
+                response1 = Console.ReadLine();
+                Console.Write("Enter your password: ");
+                response2 = Console.ReadLine();
+                if (client.LoginReturningUser(response1, response2))
+                    Console.WriteLine("Login successful");
+                else
+                    Console.WriteLine("The username and password are incorrect");
+            }
+            else
+            {
+                Console.WriteLine("Please create a new account");
+                Console.Write("Create username: ");
+                response1 = Console.ReadLine();
+                Console.Write("Create password: ");
+                response2 = Console.ReadLine();
+                client.CreateNewUser(response1, response2);
+                Console.WriteLine("New account created");
+                response1 = "y";
+                goto login;
+                //Console.Write("Re-enter password: ");
+            }
+
+            /*
+            Console.WriteLine("Please login");
+            string name = "Edwin";
+            string password = "1234";
+            client.CreateNewUser(name, password);
+            name = "wrong";
+            bool exists = client.LoginReturningUser(name, password);
+            Console.WriteLine($"This user exists: {exists}");
+            */
+        }
+    }
+}
